8fcde95e4e3f494d257b01a9d3bafed7da505bad
arundeveloper2731 <arunrajuka2001@gmail.com>
2026-07-06 12:45:24 +0530

    --

--- src/main/java/com/example/orn/util/CorsConfig.java
+++ src/main/java/com/example/orn/util/CorsConfig.java
@@ -21,7 +21,6 @@ public class CorsConfig {
                             "https://ornmanagement.netlify.app"
                         )
                         .allowedMethods("*");
-
             }
 
         };
