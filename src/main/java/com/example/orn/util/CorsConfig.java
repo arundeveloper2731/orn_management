c99cf8782504e5b5a64a4e3154d1bc960aaaeddc
arundeveloper2731 <arunrajuka2001@gmail.com>
2026-07-06 12:40:36 +0530

    --

--- src/main/java/com/example/orn/util/CorsConfig.java
+++ src/main/java/com/example/orn/util/CorsConfig.java
@@ -10,14 +10,23 @@ public class CorsConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
+
         return new WebMvcConfigurer() {
+
             @Override
             public void addCorsMappings(CorsRegistry registry) {
+
                 registry.addMapping("/**")
-                        .allowedOrigins("https://ornmanagement.netlify.app")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
+                        .allowedOrigins(
+                            "https://ornmanagement.netlify.app"
+                        )
+                        .allowedMethods("*");
+
             }
+
         };
+
     }
+
 }
+
